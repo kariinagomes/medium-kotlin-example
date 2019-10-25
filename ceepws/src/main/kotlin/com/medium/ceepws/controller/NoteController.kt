a9bace86b900cdf2e1c43140db9487e9ff689f83
Karina Gomes <kariinagomes_@hotmail.com>
2019-10-25 09:51:00 -0300

    add response entity

--- ceepws/src/main/kotlin/com/medium/ceepws/controller/NoteController.kt
+++ ceepws/src/main/kotlin/com/medium/ceepws/controller/NoteController.kt
@@ -4,6 +4,8 @@ import com.medium.ceepws.model.Note
 import com.medium.ceepws.repository.NoteRepository
 import com.medium.ceepws.service.NoteService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController //agora não precisamos mas indicar o @ResponseBody no fun list
@@ -18,27 +20,33 @@ class NoteController (private val noteService: NoteService) {
     }*/
 
     @GetMapping
-    fun list(): List<Note> {
-        return noteService.all().toList()
+    fun list(): ResponseEntity<List<Note>> {
+        val allNotes = noteService.all().toList()
+        return ResponseEntity.ok(allNotes)
     }
 
     @PostMapping
-    fun add(@RequestBody note: Note): Note {
-        return noteService.save(note)
+    fun add(@RequestBody note: Note): ResponseEntity<Note> {
+        val savedNode = noteService.save(note)
+        return ResponseEntity.ok(savedNode)
     }
 
     @PutMapping("{id}")
-    fun alter(@PathVariable id: Long, @RequestBody note: Note): Note {
+    fun alter(@PathVariable id: Long, @RequestBody note: Note): ResponseEntity<Note> {
         if (noteService.existsById(id)) {
-            return noteService.alter(id, note)
+            val updatedNode = noteService.alter(id, note)
+            return ResponseEntity.ok(updatedNode)
         }
-        return Note();
+        return ResponseEntity.notFound().build()
     }
 
     @DeleteMapping("{id}")
-    fun delete(@PathVariable id: Long) {
+    fun delete(@PathVariable id: Long): ResponseEntity<Unit> {
         if (noteService.existsById(id)) {
-            noteService.deleteById(id);
+            noteService.deleteById(id)
+            return ResponseEntity.noContent().build()
+            //return ResponseEntity.status(HttpStatus.ACCEPTED).build()
         }
+        return ResponseEntity.notFound().build()
     }
 }
